414825ba75b8270a03b6c163fd13b5326694af84
romainsilvy <romain@qr-developpement.fr>
2025-04-28 20:49:50 +0200

    :bug: display travel date when null

--- resources/views/components/travel/card.blade.php
+++ resources/views/components/travel/card.blade.php
@@ -1,11 +1,45 @@
-<a class="p-4 bg-white dark:bg-zinc-800 rounded-2xl shadow-md space-y-2" href="{{ route('travel.show', ['travelId' => $travel->id]) }}" wire:navigate>
+<a class="p-4 bg-white dark:bg-zinc-800 rounded-2xl shadow-md space-y-2"
+    href="{{ route('travel.show', ['travelId' => $travel->id]) }}" wire:navigate>
     <h2 class="text-lg font-semibold text-zinc-800 dark:text-white">
         {{ $travel->name }}
     </h2>
 
-    <p class="text-zinc-500 dark:text-zinc-400 text-sm">
-        {{ $travel->start_date->format('d/m/Y') . ' - ' . $travel->end_date->format('d/m/Y') }}
-    </p>
+    @isset($travel->start_date)
+        <p class="text-zinc-500 dark:text-zinc-400 text-sm">
+            Départ le
+            {{ $travel->start_date->format('d/m/Y') }}
+        </p>
+    @endisset
+
+    @isset($travel->end_date)
+        <p class="text-zinc-500 dark:text-zinc-400 text-sm">
+            Retour le
+            {{ $travel->end_date->format('d/m/Y') }}
+        </p>
+    @endisset
+
+    @if (isset($travel->start_date) && isset($travel->end_date))
+        <p class="text-zinc-500 dark:text-zinc-400 text-sm">
+            Du
+            {{ $travel->start_date->format('d/m/Y') }}
+            au
+            {{ $travel->end_date->format('d/m/Y') }}
+        </p>
+    @elseif (isset($travel->start_date))
+        <p class="text-zinc-500 dark:text-zinc-400 text-sm">
+            Départ le
+            {{ $travel->start_date->format('d/m/Y') }}
+        </p>
+    @elseif (isset($travel->end_date))
+        <p class="text-zinc-500 dark:text-zinc-400 text-sm">
+            Retour le
+            {{ $travel->end_date->format('d/m/Y') }}
+        </p>
+    @else
+        <p class="text-zinc-500 dark:text-zinc-400 text-sm">
+            Pas de dates définies
+        </p>
+    @endif
 
     <p class="text-zinc-600 dark:text-zinc-300">
         {{ $travel->place_name }}
