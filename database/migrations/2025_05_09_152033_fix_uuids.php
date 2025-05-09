1cd91be73479c6ca9ac101e3c1a3cbd27bba7bc2
romainsilvy <romain@qr-developpement.fr>
2025-05-09 17:25:39 +0200

    :zap: fix uuids

---
+++ database/migrations/2025_05_09_152033_fix_uuids.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('activities', function (Blueprint $table) {
+            $table->uuid('id')->primary()->change();
+        });
+
+        Schema::table('housings', function (Blueprint $table) {
+            $table->uuid('id')->primary()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('activities', function (Blueprint $table) {
+            $table->uuid('id')->change();
+        });
+
+        Schema::table('housings', function (Blueprint $table) {
+            $table->uuid('id')->change();
+        });
+    }
+};
