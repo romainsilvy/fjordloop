62a4c572b2d52dc4a7b9de71f327b4d8c04f9375
romainsilvy <romain@qr-developpement.fr>
2025-08-19 14:20:53 +0200

    :lock: add ratelimiter on login

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,12 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Blade;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Support\Facades\RateLimiter;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,6 +30,11 @@ class AppServiceProvider extends ServiceProvider
 
         Blade::directive('euro', function ($expression) {
             return "<?php echo number_format({$expression}, 2, ',', ' ') . ' €'; ?>";
+        });
+
+        RateLimiter::for('login', function (Request $request) {
+            $key = ($request->input('email') ?? 'guest') . '|' . $request->ip();
+            return Limit::perMinute(5)->by($key);
         });
     }
 }
