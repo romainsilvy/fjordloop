e80e5d7223732e0e37a1bb513e69b27ee6ccfa1b
romainsilvy <romain@qr-developpement.fr>
2025-08-19 21:15:26 +0200

    :zap: improve user email verification process

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -10,7 +10,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, HasUuids, Notifiable;
--- lang/fr/auth.php
+++ lang/fr/auth.php
@@ -16,4 +16,13 @@ return [
     'failed' => 'Ces identifiants ne correspondent pas à nos enregistrements.',
     'password' => 'Le mot de passe fourni est incorrect.',
     'throttle' => 'Trop de tentatives de connexion. Veuillez réessayer dans :seconds secondes.',
+    'verification' => [
+        'unverified' => 'Votre adresse e-mail n\'est pas vérifiée.',
+        'resend_link' => 'Cliquez ici pour renvoyer l\'e-mail de vérification.',
+        'link_sent' => 'Un nouveau lien de vérification a été envoyé à votre adresse e-mail.',
+        'registration_link_sent' => 'Un nouveau lien de vérification a été envoyé à l\'adresse e-mail que vous avez fournie lors de l\'inscription.',
+        'resend_email' => 'Renvoyer l\'e-mail de vérification',
+        'verify_message' => 'Veuillez vérifier votre adresse e-mail en cliquant sur le lien que nous venons de vous envoyer.',
+        'logout' => 'Se déconnecter',
+    ],
 ];
